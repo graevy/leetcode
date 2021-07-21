9f998cc563492388d4a01a3cd0028db231a1faee
avery <avry@pm.me>
2021-07-20 21:24:21 -0700

    2nd leetcode solution

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,6 @@
+# my first solution. kinda mediocre. i think it's O(n^2)? my second lc solution
 def longestPalindrome(s):
     def recur(s, start, stop):
-        print(s[start:stop+1])
         if not start or stop==len(s)-1:
             return (start,stop)
         if s[start-1]==s[stop+1]:
@@ -10,17 +10,22 @@ def longestPalindrome(s):
     start, stop = 0, 1
     longest, size = s[0], 1
     while stop < len(s):
-        print(start,stop)
         sub = s[start:stop+1]
-        if sub[:(len(sub)//2)] == sub[len(sub)//2:]:
+        # determines if substring is a palindrome (double negative integer division rounds up)
+        if sub[:(len(sub)//2)] == sub[-(-len(sub)//2):]:
             substart, substop = recur(s,start,stop)
-            if substop-substart > size:
-                size = substop-substart
+            if substop-substart >= size:
+                size = 1+substop-substart
                 longest = s[substart:substop+1]
+                print(f"new drome {longest} is {size} long")
         if start == stop:
             stop += 1
         else:
             start += 1
     return longest
 
+# this is a famous problem. Manacher's algorithm solves it in LINEAR TIME
+
 print(longestPalindrome('cbbd'))
+print(longestPalindrome("aba"))
+print(longestPalindrome('abbabbax'))
