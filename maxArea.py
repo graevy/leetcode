15eed82900a9ef3f014761da4623f888af21e399
avery <avry@pm.me>
2022-01-06 00:20:38 -0800

    progress toward a linear solution

--- maxArea.py
+++ maxArea.py
@@ -1,52 +1,77 @@
 import _timed
 
+
+def calc_area(arr, idx_i, idx_j):
+    # return (i if i>j else j) * abs(idx_i - idx_j)
+    i,j = arr[idx_i],arr[idx_j]
+    if i>j:
+        return j * abs(idx_i - idx_j)
+    else:
+        return i * abs(idx_i - idx_j)
+
+
 # first attempt
 # only linearly solves some basic arrays
 # needs to iterate through the list differently (from edges to middle maybe?)
 # and a more sophisticated area test when encountering new tallLines
-def maxArea1(arr):
-    if arr[0] < arr[-1]:
-        shortLine = arr[0]
-        tallLine = arr[-1]
-        tallIdx = len(arr) - 1
-    else:
-        shortLine = arr[-1]
-        tallLine = arr[0]
-        tallIdx = 0
+# def maxArea1(arr):
+#     if arr[0] < arr[-1]:
+#         shortLine = arr[0]
+#         tallLine = arr[-1]
+#         tallIdx = len(arr) - 1
+#     else:
+#         shortLine = arr[-1]
+#         tallLine = arr[0]
+#         tallIdx = 0
 
-    area = shortLine * len(arr)
+#     area = shortLine * len(arr)
 
-    print(f"shortLine = {shortLine}, tallLine = {tallLine}, tallIdx = {tallIdx}, area = {area}")
+#     for idx, elem in enumerate(arr[1:], 1):
+#         if elem > shortLine:
+#             areaTest = min(elem, tallLine) * (abs(tallIdx - idx) + 1)
+#             if areaTest > area:
+#                 area = areaTest
+#                 if elem > tallLine:
+#                     shortLine, tallLine, tallIdx = tallLine, elem, idx
+#                 else:
+#                     shortLine = elem
+#     return area
 
-    for idx, elem in enumerate(arr[1:], 1):
-        print(f"elem {elem} @ idx {idx}: ")
+# if you fail, make a brute force algorithm first
+def max_area_2(arr):
+    largest = 0
+    n = len(arr)
+    for i in range(n):
+        for j in range(n):
+            # saving calc_area is quadratic, running it twice is logarithmic
+            if calc_area(arr, i, j) > largest:
+                largest = calc_area(arr, i, j)
+    return largest
 
-        if elem > shortLine:
-            print(f"elem > shortLine")
-            areaTest = min(elem, tallLine) * (abs(tallIdx - idx) + 1)
-            if areaTest > area:
-                print(f"bigger area found: {areaTest}")
-                area = areaTest
-                if elem > tallLine:
-                    print(f"elem {elem} > tallLine")
-                    shortLine, tallLine, tallIdx = tallLine, elem, idx
-                    print(f"shortLine = {shortLine}, tallLine = {tallLine}, tallIdx = {tallIdx}, area = {area}")
-                else:
-                    print(f"elem {elem} !> tallLine")
-                    shortLine = elem
-                    print(f"shortLine = {shortLine}, tallLine = {tallLine}, tallIdx = {tallIdx}, area = {area}")
+# i slightly fine-tuned the bruteforce
+def max_area_3(arr):
+    largest = 0
+    n = len(arr)
+    for idx_i in range(n):
+        for idx_j in range(idx_i):
+            if calc_area(arr,idx_i,idx_j) > largest:
+                largest = calc_area(arr,idx_i,idx_j)
+    return largest
 
-    print("#########################")
-    return area
-
-def maxArea2(arr):
-    pass
+def max_area_4(arr):
+    n = len(arr)
+    values2 = sorted([(item * idx, item, idx) for idx,item in enumerate(arr)], reverse=True)
+    values3 = sorted([(item * (n-idx), item, idx) for idx,item in enumerate(arr)], reverse=True)
+    height = min(values2[0][1], values3[0][1])
+    width = abs(values2[0][2] - values3[0][2])
+    return height * width
 
 data = [
-    [1,8,6,2,5,4,8,3,7], # 56
-    [x for x in range(1,10)], # 25
-    [x for x in range(1,10)[::-1]], # 25
-    [1,2,9,6,8,3,4,6,7,2,9,0,1] # 81
+    [1,8,6,2,5,4,8,3,7], # 49
+    [x for x in range(1,10)], # 20
+    [x for x in range(1,10)[::-1]], # 20
+    [1,2,9,6,8,3,4,6,7,2,9,0,1] # 72
     ]
 
-timeBatch(data, maxArea1)
+_timed.timeBatch(data, max_area_4, loops=10000)
+# print(max_area_4(data[0]))
