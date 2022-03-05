2bb8d7f2322cb59a65e31e545b4b3cf0b08a1962
avery <avrypromo@pm.me>
2022-03-04 20:43:57 -0800

    passes all test cases i can throw at it

--- rotate_image.py
+++ rotate_image.py
@@ -78,8 +78,11 @@ from timing import batch
 import numpy as np
 
 
-data = [[[1,2],[3,4]], [[1,2,3],[4,5,6],[7,8,9]]]
+data = [[[1,2],[3,4]], [[1,2,3],[4,5,6],[7,8,9]], [[1]], [[]],
+    [[1,2,3,4,5,6],[7,8,9,10,11,12],[13,14,15,16,17,18],
+    [19,20,21,22,23,24],[25,26,27,28,29,30],[31,32,33,34,35,36]]
+]
+# this classification doesn't work for the empty matrix but i just ignore it
 classifiers = [[[elem for elem in line] for line in np.rot90(point, k=-1)] for point in data]
-#[[[3,1],[4,2]], ]
 
 batch(fns=[Solution().rotate], data=data, classifiers=classifiers)
